e9b3cef264471e0d75482c711b0033cfa4294df9
Npx <cheyprivado@gmail.com>
2024-10-05 00:32:21 -0300

    atualizando migration

---
+++ src/main/resources/db/migration/V1__create-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tbl_users (
+    id BINARY(16) PRIMARY KEY NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    display_name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) DEFAULT 'USER'
+);
